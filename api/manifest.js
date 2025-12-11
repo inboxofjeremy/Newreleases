eb477034992c4e7c7c705a9ece2f3bd3a70c1558
inboxofjeremy <inboxofjeremys@gmail.com>
2025-12-10 16:16:47 -0800

    Update manifest.js

--- api/manifest.js
+++ api/manifest.js
@@ -1,27 +1,26 @@
 export const config = { runtime: "edge" };
 
-const manifest = {
-  id: "recent_movies",
-  version: "1.0.0",
-  name: "Recent Movie Releases",
-  description: "Hollywood movies released in the last 90 days",
-  catalogs: [
-    {
-      id: "recent_movies",
-      type: "movie",
-      name: "Recent Releases",
-      extra: [{ name: "skip", isRequired: false }]
-    }
-  ],
-  resources: ["catalog"],
-  types: ["movie"]
-};
+export default async function handler() {
+  const manifest = {
+    id: "recent_movies",
+    version: "1.0.0",
+    name: "Recent Movie Releases",
+    description: "Hollywood movies released in the last 90 days",
+    catalogs: [
+      {
+        type: "movie",
+        id: "recent_movies",
+        name: "Recent Releases"
+      }
+    ],
+    resources: ["catalog", "meta"],
+    types: ["movie"]
+  };
 
-export default async function handler(req) {
-  return new Response(JSON.stringify(manifest), {
+  return new Response(JSON.stringify(manifest, null, 2), {
     headers: {
-      "Content-Type": "application/json",
-      "Access-Control-Allow-Origin": "*"
+      "Access-Control-Allow-Origin": "*",
+      "Content-Type": "application/json"
     }
   });
 }
