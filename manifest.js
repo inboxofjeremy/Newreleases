9f3042c7fc2ddac1ff77883839bb5a7aabbed4bc
inboxofjeremy <inboxofjeremys@gmail.com>
2025-12-10 16:51:45 -0800

    Update and rename api/manifest.js to manifest.js

---
+++ manifest.js
@@ -0,0 +1,15 @@
+{
+  "id": "hollywood-90day",
+  "version": "1.0.0",
+  "name": "Hollywood 90-Day Releases",
+  "description": "Theatrical & streaming US-region movies released within the last 90 days.",
+  "resources": ["catalog"],
+  "types": ["movie"],
+  "catalogs": [
+    {
+      "type": "movie",
+      "id": "recent_movies",
+      "name": "Recent Movies"
+    }
+  ]
+}
