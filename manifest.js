cb736d8b43aae9cc8b5213c1c8b4a9b18d4dc569
inboxofjeremy <inboxofjeremys@gmail.com>
2025-12-12 09:16:21 -0800

    Update manifest.js

--- manifest.js
+++ manifest.js
@@ -1,15 +1,15 @@
 {
-  "id": "recent_us_movies",
+  "id": "new_releases_movies",
   "version": "1.0.0",
-  "name": "Recent US Movie Releases",
-  "description": "US movies released in the last 180 days (TMDB)",
-  "resources": ["catalog", "meta"],
+  "name": "New Releases",
+  "description": "US English movie releases from the last 180 days",
   "types": ["movie"],
+  "resources": ["catalog", "meta"],
   "catalogs": [
     {
-      "id": "recent_movies",
       "type": "movie",
-      "name": "Recent Movies"
+      "id": "new_releases",
+      "name": "New Releases"
     }
   ],
   "idPrefixes": ["tmdb"]
